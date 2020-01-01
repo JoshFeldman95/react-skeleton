0e5cb75f2300b46129993477ea6265f0607000fe
Cory Lynch <cjl2625@gmail.com>
2019-12-31 19:41:15 -0500

    Rename app.js -> server.js

--- server/server.js
+++ server/server.js
@@ -7,14 +7,8 @@ const io = require("socket.io")(http);
 
 const publicPath = path.resolve(__dirname, "..", "client", "dist");
 
-
 app.use(express.static(publicPath));
 
 http.listen(3000, () => {
-  console.log(`Listening on port 3000 and looking in folder ${publicPath}`);
+  console.log(`Listening on port 3000 and serving folder ${publicPath}`);
 });
-
-
-
-
-
