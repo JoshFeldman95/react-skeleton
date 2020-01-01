b4ddc310d7619186b6d99ba18c06b1613b913c62
Cory Lynch <cjl2625@gmail.com>
2019-12-31 19:22:10 -0500

    Enable source map for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
-const path = require('path');
-const entryFile = path.resolve(__dirname, 'client', 'src', 'index.js');
-const outputDir = path.resolve(__dirname, 'client', 'dist');
+const path = require("path");
+const entryFile = path.resolve(__dirname, "client", "src", "index.js");
+const outputDir = path.resolve(__dirname, "client", "dist");
 
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
-  entry: ['@babel/polyfill', entryFile],
+  entry: ["@babel/polyfill", entryFile],
   output: {
-    publicPath:"/",
-    filename: 'bundle.js',
+    publicPath: "/",
+    filename: "bundle.js",
     path: outputDir
   },
+  devtool: "inline-source-map",
   module: {
     rules: [
-
       {
         test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/
       },
       {
         test: /\.(scss|css)$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader"
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader"
           }
         ]
       }
     ]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
-    contentBase: './client/dist',
+    contentBase: "./client/dist",
     hot: true,
     proxy: {
-      '/api': 'http://localhost:3000',
+      "/api": "http://localhost:3000"
     }
   }
 };
